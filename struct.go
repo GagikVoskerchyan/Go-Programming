6802ad7ae89e91b493179be486789bd5ac17c3e3
Gagik Voskerchan <gagikvoskerchyan@gmail.com>
2025-11-23 23:17:04 -0500

    Third day progress on Go

---
+++ struct.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+import "fmt"
+
+type Wheel struct{
+	Material string
+	Radius string
+}
+
+type car struct{
+	Make string
+	Model string
+	Weght float32
+	Wheel Wheel
+}
+
+//Anonymouse Struct is only used when you have no other reason to use it more then once
+myVehicul := struct{
+	Make string
+	Model string
+}{
+	Make: "Mercedes"
+	Model: "S63s"
+}
+
+// Embedded Struct
+
+
+func main() {
+
+	Vehicul := myVehicul{}
+	myCar := car{}
+	myCar.Wheel.Radius = "5"
+	fmt.Println("Welcome to Textio,", myCar, Vehicul)
+}
+
+*/
